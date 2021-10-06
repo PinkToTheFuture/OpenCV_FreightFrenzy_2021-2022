a6681b28942f16f57eb9836d756ac48fadff4c6c
PinkToTheFuture <pinktothefuture@gmail.com>
2021-10-06 13:18:39 +0200

    Tutorial

---
+++ ContourPipeline.java
@@ -0,0 +1,142 @@
+package org.firstinspires.ftc.teamcode.opencv;
+
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.MatOfPoint2f;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+import org.openftc.easyopencv.OpenCvPipeline;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// Credits to team 7303 RoboAvatars, adjusted by team 3954 Pink to the Future
+
+public class ContourPipeline extends OpenCvPipeline
+{
+    Scalar HOT_PINK = new Scalar(196, 23, 112);
+    // Pink, the default color                         Y      Cr     Cb
+    public static Scalar scalarLowerYCrCb = new Scalar(  0.0, 150.0, 120.0);
+    public static Scalar scalarUpperYCrCb = new Scalar(255.0, 255.0, 255.0); // pink
+
+    public boolean error = false;
+    public Exception debug;
+
+    private int borderLeftX   = 0;   //amount of pixels from the left side of the cam to skip
+    private int borderRightX  = 0;   //amount of pixels from the right of the cam to skip
+    private int borderTopY    = 0;   //amount of pixels from the top of the cam to skip
+    private int borderBottomY = 0;   //amount of pixels from the bottom of the cam to skip
+
+    private int CAMERA_WIDTH = 640;
+    private int CAMERA_HEIGHT = 360;
+
+    private int loopcounter = 0;
+    private int ploopcounter = 0;
+
+    private Mat mat = new Mat();
+    private Mat processed = new Mat();
+
+    private Rect maxRect = new Rect();
+
+    private double maxArea = 0;
+    private boolean first = false;
+
+    public void ConfigurePipeline(int borderLeftX, int borderRightX, int borderTopY, int borderBottomY, int CAMERA_WIDTH, int CAMERA_HEIGHT)
+    {
+        this.borderLeftX = borderLeftX;
+        this.borderRightX = borderRightX;
+        this.borderTopY = borderTopY;
+        this.borderBottomY = borderBottomY;
+        this.CAMERA_WIDTH = CAMERA_WIDTH;
+        this.CAMERA_HEIGHT = CAMERA_HEIGHT;
+    }
+
+    public void ConfigureScalarLower(double Y, double Cr, double Cb) { scalarLowerYCrCb = new Scalar(Y, Cr, Cb); }
+    public void ConfigureScalarUpper(double Y, double Cr, double Cb) { scalarUpperYCrCb = new Scalar(Y, Cr, Cb); }
+    public void ConfigureScalarLower(int Y, int Cr, int Cb) { scalarLowerYCrCb = new Scalar(Y, Cr, Cb); }
+    public void ConfigureScalarUpper(int Y, int Cr, int Cb) { scalarUpperYCrCb = new Scalar(Y, Cr, Cb); }
+
+    @Override
+    public Mat processFrame(Mat input)
+    {
+        Mat output = input.clone();
+        try
+        {
+            // Process Image
+            Imgproc.cvtColor(input, mat, Imgproc.COLOR_RGB2YCrCb);
+            Core.inRange(mat, scalarLowerYCrCb, scalarUpperYCrCb, processed);
+            // Core.bitwise_and(input, input, output, processed);
+
+            // Remove Noise
+            Imgproc.morphologyEx(processed, processed, Imgproc.MORPH_OPEN, new Mat());
+            Imgproc.morphologyEx(processed, processed, Imgproc.MORPH_CLOSE, new Mat());
+            // GaussianBlur not tested yet!
+            Imgproc.GaussianBlur(processed, processed, new Size(5.0, 15.0), 0.00);
+            // Find Contours
+            List<MatOfPoint> contours = new ArrayList<>();
+            Imgproc.findContours(processed, contours, new Mat(), Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_SIMPLE);
+
+            // Draw Contours
+            Imgproc.drawContours(output, contours, -1, new Scalar(255, 0, 0));
+
+            // Loop Through Contours
+            for (MatOfPoint contour : contours)
+            {
+                Point[] contourArray = contour.toArray();
+
+                // Bound Rectangle if Contour is Large Enough
+                if (contourArray.length >= 15)
+                {
+                    MatOfPoint2f areaPoints = new MatOfPoint2f(contourArray);
+                    Rect rect = Imgproc.boundingRect(areaPoints);
+
+                    // if rectangle is larger than previous cycle or if rectangle is not larger than previous 6 cycles > then replace
+                    if (rect.area() > maxArea
+                            && rect.x > borderLeftX && rect.x + rect.width < CAMERA_WIDTH - borderRightX
+                            && rect.y > borderTopY && rect.y + rect.height < CAMERA_HEIGHT - borderBottomY
+                            || loopcounter - ploopcounter > 6)
+                    {
+                        maxArea = rect.area();
+                        maxRect = rect;
+                        ploopcounter++;
+                        loopcounter = ploopcounter;
+                        first = true;
+                    }
+                }
+            }
+            if (contours.isEmpty())
+            {
+                maxRect = new Rect();
+            }
+            // Draw Rectangles If Area Is At Least 500
+            if (first && maxRect.area() > 500)
+            {
+                Imgproc.rectangle(output, maxRect, new Scalar(0, 255, 0), 2);
+            }
+            // Draw Borders
+            Imgproc.rectangle(output, new Rect(borderLeftX, borderTopY, CAMERA_WIDTH - borderRightX - borderLeftX, CAMERA_HEIGHT - borderBottomY - borderTopY), HOT_PINK, 2);
+            // Display Data
+            Imgproc.putText(output, "Area: " + getRectArea() + " Midpoint: " + getRectMidpointXY().x + " , " + getRectMidpointXY().y, new Point(5, CAMERA_HEIGHT - 5), 0, 0.6, new Scalar(255, 255, 255), 2);
+
+            loopcounter++;
+        } catch (Exception e) {
+            debug = e;
+            error = true;
+        }
+
+        return output;
+    }
+    public int getRectHeight(){return maxRect.height;}
+    public int getRectWidth(){ return maxRect.width; }
+    public int getRectX(){ return maxRect.x; }
+    public int getRectY(){ return maxRect.y; }
+    public double getRectMidpointX(){ return getRectX() + (getRectWidth()/2.0); }
+    public double getRectMidpointY(){ return getRectY() + (getRectHeight()/2.0); }
+    public Point getRectMidpointXY(){ return new Point(getRectMidpointX(), getRectMidpointY());}
+    public double getAspectRatio(){ return getRectArea()/(CAMERA_HEIGHT*CAMERA_WIDTH); }
+    public double getRectArea(){ return maxRect.area(); }
+}
---
+++ OpenCV_Tutorial_Freight_Frenzy.java
@@ -0,0 +1,125 @@
+package org.firstinspires.ftc.teamcode.opencv;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.opencv.core.Scalar;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+
+@Config
+@Autonomous(name="OpenCV_Test", group="Tutorials")
+
+public class OpenCV_Tutorial_Freight_Frenzy extends LinearOpMode {
+
+    private OpenCvCamera webcam;
+
+    private static final int CAMERA_WIDTH  = 640; // width  of wanted camera resolution
+    private static final int CAMERA_HEIGHT = 360; // height of wanted camera resolution
+
+    double CrLowerUpdate = 150;
+    double CbLowerUpdate = 120;
+    double CrUpperUpdate = 255;
+    double CbUpperUpdate = 255;
+
+    double lowerruntime = 0;
+    double upperruntime = 0;
+
+    // Pink Range                                      Y      Cr     Cb
+    public static Scalar scalarLowerYCrCb = new Scalar(  0.0, 150.0, 120.0);
+    public static Scalar scalarUpperYCrCb = new Scalar(255.0, 255.0, 255.0);
+
+    @Override
+    public void runOpMode()
+    {
+        // OpenCV webcam
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
+        //OpenCV Pipeline
+        ContourPipeline myPipeline;
+        webcam.setPipeline(myPipeline = new ContourPipeline());
+        // Configuration of Pipeline
+        myPipeline.ConfigurePipeline(30, 30,30,30,  CAMERA_WIDTH, CAMERA_HEIGHT);
+        myPipeline.ConfigureScalarLower(scalarLowerYCrCb.val[0],scalarLowerYCrCb.val[1],scalarLowerYCrCb.val[2]);
+        myPipeline.ConfigureScalarUpper(scalarUpperYCrCb.val[0],scalarUpperYCrCb.val[1],scalarUpperYCrCb.val[2]);
+        // Webcam Streaming
+        webcam.openCameraDeviceAsync(() -> webcam.startStreaming(CAMERA_WIDTH, CAMERA_HEIGHT, OpenCvCameraRotation.UPRIGHT));
+
+        // Only if you are using ftcdashboard
+        FtcDashboard dashboard = FtcDashboard.getInstance();
+        telemetry = dashboard.getTelemetry();
+        FtcDashboard.getInstance().startCameraStream(webcam, 10);
+
+        telemetry.update();
+        waitForStart();
+
+        while (opModeIsActive())
+        {
+            if(myPipeline.error){
+                telemetry.addData("Exception: ", myPipeline.debug);
+            }
+            // Only use this line of the code when you want to find the lower and upper values
+            // testing(myPipeline);
+
+            // Watch our YouTube Tutorial for the better explanation
+
+            telemetry.addData("RectArea: ", myPipeline.getRectArea());
+            telemetry.update();
+
+            if(myPipeline.getRectArea() > 2000){
+                if(myPipeline.getRectMidpointX() > 400){
+                    AUTONOMOUS_C();
+                }
+                else if(myPipeline.getRectMidpointX() > 200){
+                    AUTONOMOUS_B();
+                }
+                else {
+                    AUTONOMOUS_A();
+                }
+            }
+        }
+    }
+    public void testing(ContourPipeline myPipeline){
+        if(lowerruntime + 0.05 < getRuntime()){
+            CrLowerUpdate += -gamepad1.left_stick_y;
+            CbLowerUpdate += gamepad1.left_stick_x;
+            lowerruntime = getRuntime();
+        }
+        if(upperruntime + 0.05 < getRuntime()){
+            CrUpperUpdate += -gamepad1.right_stick_y;
+            CbUpperUpdate += gamepad1.right_stick_x;
+            upperruntime = getRuntime();
+        }
+
+        CrLowerUpdate = inValues(CrLowerUpdate, 0, 255);
+        CrUpperUpdate = inValues(CrUpperUpdate, 0, 255);
+        CbLowerUpdate = inValues(CbLowerUpdate, 0, 255);
+        CbUpperUpdate = inValues(CbUpperUpdate, 0, 255);
+
+        myPipeline.ConfigureScalarLower(0.0, CrLowerUpdate, CbLowerUpdate);
+        myPipeline.ConfigureScalarUpper(255.0, CrUpperUpdate, CbUpperUpdate);
+
+        telemetry.addData("lowerCr ", (int)CrLowerUpdate);
+        telemetry.addData("lowerCb ", (int)CbLowerUpdate);
+        telemetry.addData("UpperCr ", (int)CrUpperUpdate);
+        telemetry.addData("UpperCb ", (int)CbUpperUpdate);
+    }
+    public Double inValues(double value, double min, double max){
+        if(value < min){ value = min; }
+        if(value > max){ value = max; }
+        return value;
+    }
+    public void AUTONOMOUS_A(){
+        telemetry.addLine("Autonomous A");
+    }
+    public void AUTONOMOUS_B(){
+        telemetry.addLine("Autonomous B");
+    }
+    public void AUTONOMOUS_C(){
+        telemetry.addLine("Autonomous C");
+    }
+}
